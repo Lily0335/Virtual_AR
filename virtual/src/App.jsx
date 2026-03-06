c47be02bddd90450ce42c94136d21b4ebe852427
lily0335 <laraibanwar00002@gmail.com>
2026-03-06 17:20:04 +0500

    commit

--- virtual/src/App.jsx
+++ virtual/src/App.jsx
@@ -1,11 +1,12 @@
+import React from "react";
+import Navbar from "./components/Navbar";
 
-import Navbar from './components/Navbar'
-const App = () => {
+function App() {
   return (
     <>
       <Navbar />
     </>
   );
-};
+}
 
-export default App
+export default App;
--- virtual/src/components/Navbar.jsx
+++ virtual/src/components/Navbar.jsx
@@ -1,15 +1,26 @@
-import logo from '../assets/logos/logo.png'
+import React from "react";
+import logo from '../assets/logos/logo.png';
+import { navItems } from "../constants";
+
 const Navbar = () => {
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border border-neutral-700/80">
         <div className="container px-4 mx-auto relative text-sm">
             <div className="container flex items-center items-center">
                 <div className="flex items-center flex-shrink-0">
-                    <img src={logo} alt="logo" />
+                    <img className="h-10 w-10 mr-2" src={logo} alt="logo" />
+                    <span className="text-xl tracking-tight">VirtualR</span>
                 </div>
-            
+                <ul className="hidden lg:flex ml-14 space-x-12">
+                    {navItems.map((item, index) => (
+                        <li key={index}>
+                            <a href={item.href} >
+                                {item.label}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
             </div>
-
         </div>
     </nav>
   )
